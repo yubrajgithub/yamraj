5b0b7411cd0bf023ff6dd648b0a4accb0ce38dec
Yubrajkhadka <127959581+yubrajgithub@users.noreply.github.com>
2024-07-24 11:35:25 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSocialMedia() {
+  window.open("https://www.linkedin.com/in/yubraj-khadka-98139820b/", "_blank");
+  window.open("https://instagram.com", "_blank");
+  // Add more social media links as needed
+}
+
+function scrollToSection(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+}
